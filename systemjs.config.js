f6631d13ff90d307967c9ec13c16599d8f3fda24
SanyaZoom <alexandrsemenets1991@gmail.com>
2017-01-25 16:17:32 +0200

    add moment.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -13,31 +13,41 @@
       // our app is within the app folder
       app: './app',
       // angular bundles
-      '@angular/core':                     'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common':                   'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler':                 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser':         'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http':                     'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router':                   'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms':                    'npm:@angular/forms/bundles/forms.umd.js',
-      // other libraries
-      'rxjs':                              'npm:rxjs',
-      'angular-in-memory-web-api':         'npm:angular-in-memory-web-api'
+        '@angular/core':                     'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common':                   'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler':                 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser':         'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http':                     'npm:@angular/http/bundles/http.umd.js',
+        '@angular/router':                   'npm:@angular/router/bundles/router.umd.js',
+        '@angular/forms':                    'npm:@angular/forms/bundles/forms.umd.js',
+        // other libraries
+        'rxjs':                              'npm:rxjs',
+        'angular-in-memory-web-api':         'npm:angular-in-memory-web-api',
+        'moment':                            'npm:moment',
+        'angular2-moment':                   'npm:angular2-moment'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
-      app: {
-        main: './main.js',
-        defaultExtension: 'js'
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      },
-      'angular-in-memory-web-api': {
-        main: './index.js',
-        defaultExtension: 'js'
-      }
+        app: {
+          main: './main.js',
+          defaultExtension: 'js'
+        },
+        rxjs: {
+          defaultExtension: 'js'
+        },
+        'angular-in-memory-web-api': {
+          main: './index.js',
+          defaultExtension: 'js'
+        },
+        'moment': {
+          main: './moment.js',
+          defaultExtension: 'js'
+        },
+        'angular2-moment': {
+          main: './index.js',
+          defaultExtension: 'js'
+        }
     }
   });
 })(this);
